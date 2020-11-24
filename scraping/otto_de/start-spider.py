c04b998cc2a03db9d367a7528f9324058f091df8
Bastian Hunecke <bastian.hunecke@icloud.com>
2020-11-24 19:42:07 +0100

    add rest api for on demand otto.de scraper

--- scraping/otto_de/start-spider.py
+++
@@ -1,8 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-from otto_de.spiders.otto_de_spider import OttoReviewsSpider
-
-process = CrawlerProcess(get_project_settings())
-process.crawl(OttoReviewsSpider, productId=723800867)
-process.start()
